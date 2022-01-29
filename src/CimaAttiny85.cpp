99ee41ea709b134d50303ade42aa4b3c2e3dc3d4
Martin Šimek <cima.m@seznam.cz>
2022-01-29 01:43:54 +0100

    3 PWMS with 3 phase rgb led driving.

--- src/CimaAttiny85.cpp
+++ src/CimaAttiny85.cpp
@@ -16,39 +16,63 @@ uint16_t phase16bit = 0;
 bool stop = false;
 
 void initTimer1(){
+  PLLCSR |= (1 << PLLE) | (1 << PCKE);
+
   TCCR1 |= (1 << CTC1);  // clear timer on compare match
   TCCR1 |= (1 << CS13) | (1 << CS12); //clock prescaler 4096
-  TCCR1 |= (1 << CS13) | (1 << CS12) | (1 << CS11); //clock prescaler 8192
-  OCR1C = 122; // compare match value 
+  //TCCR1 |= (1 << CS13) | (1 << CS12) | (1 << CS11); //clock prescaler 8192
+  //TCCR1 |= (1 << CS13) | (1 << CS12) | (1 << CS11) | (1 << CS10); //clock prescaler 16k
+  OCR1C = 255; // compare match value: if TCNT1 == 122 => compare match => call interupt
   TIMSK |= (1 << OCIE1A); // enable compare match interrupt
+/*
+  GTCCR &= ~(1 << COM1B0); //Disable time compare match output to pin PB4
+  GTCCR &= ~(1 << COM1B1);
+  GTCCR &= ~(1 << FOC1B);
+*/
 }
 
 void initPWM0() {
     /* - set OC0A on match, clear at bottom (inverting mode) 
-       - disconnect OC0B 
+       - set OC0B on match, clear at bottom (inverting mode) 
        - Fast PWM mode */
-    TCCR0A = 0b11 << COM0A0 | 0b00 << COM0B0 | 0b011 << WGM00;
+    TCCR0A = 0b11 << COM0A0 | 0b11 << COM0B0 | 0b011 << WGM00;
 
-
+    /* TODO comments */
     TCCR0B = 0b0 << WGM02 | 0b010 << CS00;
+
+    /* - Enable PWM on global timer  
+       - set the OC1B output line when comparison matches*/
+    GTCCR = 1 << PWM1B | 0b11 << COM1B0;
+
+    /* - enable PWM (1 << PWM1A)
+       - This must be set with respect to HW design bug */
+    TCCR1 |= 1 << PWM1A | 0b11 << COM1A0;
+}
+
+uint8_t phaseToIntensity(uint16_t phase){
+    uint8_t down = (0b1 & (phase >> 8));
+    
+    return 255 - (down ? (255 - phase) : phase);
 }
 
 ISR(TIMER1_COMPA_vect){
     uint8_t phase = phase16bit & 0xFF;
-    
+
     if(phase % 8 == 0){
+        /**
         if(position & messageR){
             PORTB |= (1<<PB1);      //PortB1 high 
         } else {
             PORTB &= ~(1<<PB1);     //PORTB1 low   
         }
-
+        */
+/*
         if(position & messageB){
             PORTB |= (1<<PB4);      //PortB4 high 
         } else {
             PORTB &= ~(1<<PB4);     //PORTB4 low   
         }
-
+*/
         if(position & messageG){
             PORTB |= (1<<PB2);      //PortB2 high 
         } else {
@@ -58,13 +82,15 @@ ISR(TIMER1_COMPA_vect){
         position = position << 1;
         position = position ? position : 1;
     }
-
-    uint8_t down = (0b1 & (phase16bit >> 8));
     
-    OCR0A = 255 - (down ? (255 - phase) : phase);
+    OCR0A = phaseToIntensity(phase16bit);
+    OCR0B = phaseToIntensity(phase16bit + 70);
+    OCR1B = phaseToIntensity(phase16bit + 180);
     
     phase16bit++;
 }
+
+
 
 int main() {
     
@@ -77,7 +103,11 @@ int main() {
     OCR0A = 128;
 
     PORTB |= (1<<PB1);      //PortB1 high 
+    OCR0B = 128;
+
     PORTB |= (1<<PB4);      //PortB4 high 
+    OCR1B = 128;
+
     PORTB |= (1<<PB2);      //PortB2 high 
 
     //PORTB&=~(1<<PB1);     //PORTB1 low 
